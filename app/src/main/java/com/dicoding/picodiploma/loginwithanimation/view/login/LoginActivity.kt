ea75ec343432313160e4590b2b9940545afab3ec
LutfiEka <lutfiekj@gmail.com>
2024-12-16 09:47:31 +0700

    Remove Unused Import

--- app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/login/LoginActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/login/LoginActivity.kt
@@ -2,7 +2,6 @@ package com.dicoding.picodiploma.loginwithanimation.view.login
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -60,7 +59,6 @@ class LoginActivity : AppCompatActivity() {
 
                         viewModel.saveSession(user)
 
-                        Log.d("LoginActivity", "Token saved successfully")
                         startActivity(Intent(this@LoginActivity, HomeActivity::class.java))
                         finish()
                     }
